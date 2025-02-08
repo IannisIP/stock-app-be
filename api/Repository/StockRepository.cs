c80eb1a55f35df57e58027b1c2651db97c6e171b
Iannis Paris <parisiannis16@stud.ase.ro>
2025-02-08 13:05:38 +0200

    Add basic crud operations

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -1,4 +1,5 @@
 ﻿using api.Data;
+using api.Dtos.Stock;
 using api.Models;
 using api.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,13 @@ namespace api.Repository
             _dBContext = dBContext;
         }
 
+        public async Task<Stock> AddAsync(Stock stock)
+        {
+            await _dBContext.Stocks.AddAsync(stock);
+            await _dBContext.SaveChangesAsync();
+            return stock;
+        }
+
         public async Task<List<Stock>> GetAllAsync()
         {
             return await _dBContext.Stocks.ToListAsync();
@@ -21,7 +29,36 @@ namespace api.Repository
 
         public async Task<Stock> GetByIdAsync(int id)
         {
-            return await _dBContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            var stock = await _dBContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stock == null)
+            {
+                throw new KeyNotFoundException($"Stock with id {id} not found.");
+            }
+            return stock;
+        }
+
+        public async Task<Stock> UpdateAsync(int id, UpdateStockDto updateStockDto)
+        {
+            var stockToUpdate = await GetByIdAsync(id);
+
+            stockToUpdate.Industry = updateStockDto.Industry;
+            stockToUpdate.CompanyName = updateStockDto.CompanyName;
+            stockToUpdate.MarketCap = updateStockDto.MarketCap;
+            stockToUpdate.LastDiv = updateStockDto.LastDiv;
+            stockToUpdate.Purchase = updateStockDto.Purchase;
+            stockToUpdate.Symbol = updateStockDto.Symbol;
+
+
+            await _dBContext.SaveChangesAsync();
+            return stockToUpdate;
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var stock = await GetByIdAsync(id);
+
+            _dBContext.Stocks.Remove(stock);
+            return await _dBContext.SaveChangesAsync();
         }
     }
 }
--- api/Services/StockService.cs
+++ api/Services/StockService.cs
@@ -1,4 +1,6 @@
-﻿using api.Models;
+﻿using api.Dtos.Stock;
+using api.Mappers;
+using api.Models;
 using api.Repository;
 using api.Repository.Interfaces;
 using api.Services.Interfaces;
@@ -14,16 +16,40 @@ namespace api.Services
             _stockRepository = stockRepository;
         }
 
-        public async Task<List<Stock>> GetAllAsync()
+        public async Task<List<StockDto>> GetAllAsync()
         {
-            var stocks = await _stockRepository.GetAllAsync();
+            var query = await _stockRepository.GetAllAsync();
+
+            var stocks = query.Select(s => s.ToStockDto()).ToList();
+
             return stocks;
         }
 
-        public async Task<Stock> GetByIdAsync(int id) 
+        public async Task<StockDto> GetByIdAsync(int id) 
         {
-            var stock = await _stockRepository.GetByIdAsync(id);
+            var query = await _stockRepository.GetByIdAsync(id);
+            var stock = query.ToStockDto();
+
             return stock;
+        }
+
+        public async Task<StockDto> AddAsync(CreateStockDto dto)
+        {
+            
+            var stock = await _stockRepository.AddAsync(dto.toStockFromCreateDto());
+            return stock.ToStockDto();
+        }
+
+        public async Task<StockDto> UpdateAsync(int id, UpdateStockDto updateStockDto)
+        {
+            var stock = await _stockRepository.UpdateAsync(id, updateStockDto);
+
+            return stock.ToStockDto();
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            return await _stockRepository.DeleteAsync(id);
         }
     }
 }
